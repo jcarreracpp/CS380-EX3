efd422e702a44becb14d6859cfeabce474853280
Jorge Carrera <Jorge Carrera>
2017-04-24 22:19:20 -0700

    Working version, cleaned up.

--- Ex3Client.java
+++ Ex3Client.java
@@ -3,12 +3,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Jorge
@@ -29,55 +23,58 @@ public class Ex3Client {
             System.out.println("Data received: ");
             byte[] dataholder = new byte[buffer];
             
+            System.out.print("\t");
+            
             for(int i = 0; i < buffer; i++){
                 dataholder[i] = (byte)is.read();
-//                System.out.printf("%02X", dataholder[i]);
-//                if((i+1)%10 == 0){
-//                    System.out.println();
-//                }
+                System.out.printf("%02X", dataholder[i]);
+                if((i+1)%10 == 0){
+                    System.out.println();
+                    System.out.print("\t");
+                }
             }
             System.out.println("\n");
             
             
             response = checksum(dataholder, buffer);
-            System.out.println(response);
 
-            os.write((byte)(response >>> 8));
-            os.write((byte)(response));
+            os.write((short)(response >>> 8));
+            os.write((response & 0xFF));
+            
             answer = is.read();
-            System.out.print("Succeed? " + answer +"\n");
+            
+            if(answer == 1){
+                System.out.println("Response good.");
+            }else{
+                System.out.println("Response bad.");
+            }
 
             }
         }
+    
         public static short checksum(byte[] b, int counter){
             long sum = 0;
             long a;
             long c;
-            int overflow = 0;
             
             for(int i = 0; i < counter; i += 2){
-
                 if((counter - i) > 1){
                     a = (b[i] & 0xFF);
                     a <<= 8;
-                    c = (b[(i+1)] & 0xFFFF);
+                    c = (b[(i+1)] & 0xFF);
                     a += c;
-                    sum = (short)(sum + a);
+                    sum = (sum + a);
                 }else{
-                    System.out.println("ONCE");
                     a = (b[i] & 0xFF);
                     a <<= 8;
-                    sum += (short)a;
+                    sum = (sum + a);
                 }
                 
-                if((sum & 0xFFFF0000) != 0x00000000){
-                    overflow++;
+                if((sum & 0xFFFF0000) != 0){
                     sum &= 0xFFFF;
                     sum++;
                 }
-
             }
-            System.out.println("OVERFLOWED " + overflow + " TIME(S)");
-            return (short)~(sum & 0xFFFF);
+            return (short)~(sum &= 0xFFFF);
         }
 }
